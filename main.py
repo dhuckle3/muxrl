28108d3c0ee0ffe8250571b6c789a8ed44e5e31d
Donald Huckle <dhuckle3@gmail.com>
2018-03-09 14:29:49 -0500

    Implement multiple dungeon controls
    The user can now move characters in all dungeons at once or in a single dungeon. CTRL+A activates terminal control mode, while in control mode entering the number of a terminal will set you to control only that terminal. entering a will cause you to select all terminals.

--- main.py
+++ main.py
@@ -5,32 +5,84 @@ from dungeon_view import DungeonView
 
 class Main:
     def __init__(self):
+
+        self.control_active = False
         self.views = []
         self.views.append(DungeonView(3, 3, 13, 13, Dungeon(11, 11), 1))
         self.views.append(DungeonView(15, 3, 13, 13, Dungeon(11, 11), 2))
         self.views.append(DungeonView(27, 3, 13, 13, Dungeon(11, 11), 3))
         self.views.append(DungeonView(39, 3, 13, 13, Dungeon(11, 11), 4))
-
         self.viewport_width = 80
         self.viewport_height = 50
         tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
         tcod.console_init_root(self.viewport_width, self.viewport_height, 'muxRL', False)
         self.console = tcod.console_new(self.viewport_width, self.viewport_height)
+        self.selected_player = None
 
     def handle_keys(self, key):
         if key.vk == tcod.KEY_ENTER and key.lalt:
             tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
         elif key.vk == tcod.KEY_ESCAPE:
             return True
-        player = self.views[0].dungeon.player
-        if tcod.console_is_key_pressed(tcod.KEY_UP):
-            player.move(0, -1)
-        elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
-            player.move(0, 1)
-        elif tcod.console_is_key_pressed(tcod.KEY_LEFT):
-            player.move(-1, 0)
-        elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
-            player.move(1, 0)
+        if key.vk == tcod.KEY_UP or tcod.console_is_key_pressed(tcod.KEY_KP8):
+            self.move_north()
+        elif key.vk == tcod.KEY_KP9:
+            self.move_northeast()
+        elif key.vk == tcod.KEY_RIGHT or key.vk == tcod.KEY_KP6:
+            self.move_east()
+        elif key.vk == tcod.KEY_KP3:
+            self.move_southeast()
+        elif key.vk == tcod.KEY_DOWN or key.vk == tcod.KEY_KP2:
+            self.move_south()
+        elif key.vk == tcod.KEY_KP1:
+            self.move_southwest()
+        elif key.vk == tcod.KEY_LEFT or key.vk == tcod.KEY_KP4:
+            self.move_west()
+        elif key.vk == tcod.KEY_KP7:
+            self.move_northwest()
+        elif key.vk == tcod.KEY_CONTROL and key.c == ord('a'):
+            self.control_active = True
+        elif key.c == ord('a'):
+            self.selected_player = None
+            print('selecting all')
+        elif key.c >= ord('1') and key.c <= ord('9'):
+            self.control_active = False
+            index = int(chr(key.c)) - 1
+            if index in range(len(self.views)):
+                print('selecting', index)
+                self.selected_player = self.views[index].dungeon.player
+
+    def move_player(self, x, y):
+        if self.selected_player is None:
+            for view in self.views:
+                player = view.dungeon.player
+                player.move(x, y)
+        else:
+            self.selected_player.move(x, y)
+
+    def move_north(self):
+        self.move_player(0, -1)
+
+    def move_northeast(self):
+        self.move_player(1, -1)
+
+    def move_east(self):
+        self.move_player(1, 0)
+
+    def move_southeast(self):
+        self.move_player(1, 1)
+
+    def move_south(self):
+        self.move_player(0, 1)
+
+    def move_southwest(self):
+        self.move_player(-1, 1)
+
+    def move_west(self):
+        self.move_player(-1, 0)
+
+    def move_northwest(self):
+        self.move_player(-1, -1)
 
     def run(self):
         while not tcod.console_is_window_closed():
